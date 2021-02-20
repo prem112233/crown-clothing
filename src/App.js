5b568be112807d085357bbfef78ea39ee89b2ae3
prem <prem1481996@gmail.com>
2021-02-20 22:05:16 +0530

    git push

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import HomePage from'./homepage.component'
-const App=()=>{
-  return(
+import React from "react";
+import HomePage from "./pages/homepage/homepage.component";
+// import {MenuItem} from './components/menu.item/menu.item.component'
+const App = () => {
+  return (
     <div>
       <HomePage />
+      {/* <MenuItem /> */}
     </div>
-  )
-}
+  );
+};
 export default App;
